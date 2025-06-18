88a4de2f63b80a4bd44a3c02d022d23cc0450f50
xbill work <xbill@glitnir.com>
2025-06-18 22:41:07 +0000

    clean firebase

--- src/firebase/config.ts
+++
@@ -1,2 +0,0 @@
-// This file is no longer needed as Firebase dependencies have been removed.
-// You can safely delete this file.
--- src/firebase/non-blocking-login.tsx
+++
@@ -1,2 +0,0 @@
-// This file is no longer needed as authentication has been removed.
-// You can safely delete this file from your project.
